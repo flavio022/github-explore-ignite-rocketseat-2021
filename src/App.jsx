f86ae331ae55f6416e2ed20cd441cda6e902a94e
Flavio <flavio@MacBook-Air-de-Flavio.local>
2022-07-20 20:45:30 -0300

    add ReactRefreshWebpackPlugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Counter } from "./compontes/Counter";
 import { RepositoryList } from "./compontes/RepositoryList";
+import './styles/global.scss'
 
 function App(){
     return (
         <>
-            <RepositoryList/>
-            <Counter/>
+        <RepositoryList/>
+        <Counter/>
         </>
     )
 }
